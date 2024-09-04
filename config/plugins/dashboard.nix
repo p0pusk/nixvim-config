e4883167c7acdc5e4da673cdccd3377fe32b1dda
cringulya <s0sis0n@yandex.ru>
2024-09-04 15:29:04 +0300

    dashboard updates

--- config/plugins/dashboard.nix
+++ config/plugins/dashboard.nix
@@ -1,12 +1,62 @@
 {
-  plugins.dashboard = {
+  plugins.dashboard = let
+
+    dashboard_custom_header4 = [
+      ""
+      ""
+      ""
+      ""
+      "██╗░░██╗███████╗███╗░░██╗████████╗░█████╗░██╗"
+      "██║░░██║██╔════╝████╗░██║╚══██╔══╝██╔══██╗██║"
+      "███████║█████╗░░██╔██╗██║░░░██║░░░███████║██║"
+      "██╔══██║██╔══╝░░██║╚████║░░░██║░░░██╔══██║██║"
+      "██║░░██║███████╗██║░╚███║░░░██║░░░██║░░██║██║"
+      "╚═╝░░╚═╝╚══════╝╚═╝░░╚══╝░░░╚═╝░░░╚═╝░░╚═╝╚═╝"
+      ""
+      ""
+      ""
+      ""
+      ""
+    ];
+  in {
     enable = true;
 
     settings = {
       theme = "doom";
       config = {
         disable_move = true;
+        header = dashboard_custom_header4;
+        center = [
+          {
+            icon = "";
+            icon_hl = "number";
+            desc = "recent";
+            desc_hl = "group";
+            key = "r";
+            key_hl = "group";
+            action = "Telescope oldfiles";
+          }
+          {
+            icon = "";
+            icon_hl = "group";
+            desc = "projects";
+            desc_hl = "group";
+            key = "p";
+            key_hl = "group";
+            action = "Telescope projects";
+          }
+          {
+            icon = "";
+            icon_hl = "group";
+            desc = "dotfiles";
+            desc_hl = "group";
+            key = "d";
+            key_hl = "group";
+            action = "Telescope find_files cwd=~/.config";
+          }
+        ];
 
+        footer = [ ];
       };
     };
   };
