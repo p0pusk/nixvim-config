f82375046de462471e2814e7b60407c9a2a2222a
cringulya <s0sis0n@yandex.ru>
2024-09-05 21:27:12 +0300

    texlivePackage updates

--- config/plugins/vimtex.nix
+++ config/plugins/vimtex.nix
@@ -1,9 +1,14 @@
+{ pkgs, ... }:
+
 {
   plugins.vimtex = {
     enable = true;
     settings = {
       view_method = "zathura";
       compiler_latexmk.out_dir = "./build";
+      texlivePackage = (pkgs.texlive.combine {
+        inherit (pkgs.texlive) scheme-full babel-russian;
+      });
     };
   };
 }
