ab55f3a3a2eb27c490b3f7d3bbb80bb6d72f9125
cringulya <s0sis0n@yandex.ru>
2024-09-07 22:12:44 +0300

    changed vimtex compiler to latexmk

--- config/plugins/vimtex.nix
+++ config/plugins/vimtex.nix
@@ -5,7 +5,6 @@
     enable = true;
     texlivePackage = pkgs.texlive.combined.scheme-full;
     settings = {
-      compiler_method = "latexrun";
       view_method = "zathura";
       compiler_latexmk.out_dir = "./build";
       compiler_latexrun.out_dir = "./build";
