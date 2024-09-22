b2ff483916629961c8be87c5fdf302f11e5d900f
cringulya <s0sis0n@yandex.ru>
2024-09-22 23:54:19 +0300

    updates

--- snippets/tex/symbols.lua
+++ snippets/tex/symbols.lua
@@ -9,49 +9,60 @@ local fmt = require('luasnip.extras.fmt').fmt
 local fmta = require('luasnip.extras.fmt').fmta
 local rep = require('luasnip.extras').rep
 
-local tex_utils = {}
-tex_utils.in_mathzone = function() -- math context detection
+local env = {}
+env.in_mathzone = function() -- math context detection
   return vim.fn['vimtex#syntax#in_mathzone']() == 1
 end
-tex_utils.in_text = function()
-  return not tex_utils.in_mathzone()
+env.in_text = function()
+  return not env.in_mathzone()
 end
-tex_utils.in_comment = function() -- comment detection
+env.in_comment = function() -- comment detection
   return vim.fn['vimtex#syntax#in_comment']() == 1
 end
-tex_utils.in_env = function(name) -- generic environment detection
+env.in_env = function(name) -- generic environment detection
   local is_inside = vim.fn['vimtex#env#is_inside'](name)
   return (is_inside[1] > 0 and is_inside[2] > 0)
 end
 -- A few concrete environments---adapt as needed
-tex_utils.in_equation = function() -- equation environment detection
-  return tex_utils.in_env('equation')
+env.in_equation = function() -- equation environment detection
+  return env.in_env('equation')
 end
-tex_utils.in_itemize = function() -- itemize environment detection
-  return tex_utils.in_env('itemize')
+env.in_itemize = function() -- itemize environment detection
+  return env.in_env('itemize')
 end
-tex_utils.in_tikz = function() -- TikZ picture environment detection
-  return tex_utils.in_env('tikzpicture')
+env.in_tikz = function() -- TikZ picture environment detection
+  return env.in_env('tikzpicture')
 end
 
 return {
+  s(
+    { trig = ';ld' },
+    { t('\\ldotp') },
+    { condition = env.in_mathzone, show_condition = env.in_mathzone }
+  ),
+
   s({ trig = ';e' }, { t('\\varepsilon') }, {
-    condition = tex_utils.in_mathzone,
-    show_condition = tex_utils.in_mathzone,
+    condition = env.in_mathzone,
+    show_condition = env.in_mathzone,
+  }),
+
+  s({ trig = ';b' }, { t('\\beta') }, {
+    condition = env.in_mathzone,
+    show_condition = env.in_mathzone,
   }),
 
   s({ trig = ';a' }, { t('\\alpha') }, {
-    condition = tex_utils.in_mathzone,
-    show_condition = tex_utils.in_mathzone,
+    condition = env.in_mathzone,
+    show_condition = env.in_mathzone,
   }),
 
   s({ trig = ';l' }, { t('\\lambda') }, {
-    condition = tex_utils.in_mathzone,
-    show_condition = tex_utils.in_mathzone,
+    condition = env.in_mathzone,
+    show_condition = env.in_mathzone,
   }),
 
   s({ trig = ';inf' }, { t('\\infty') }, {
-    condition = tex_utils.in_mathzone,
-    show_condition = tex_utils.in_mathzone,
+    condition = env.in_mathzone,
+    show_condition = env.in_mathzone,
   }),
 }
