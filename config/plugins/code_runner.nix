fca52ec7e644289e226b280748c0603415a5e8f9
cringulya <s0sis0n@yandex.ru>
2024-10-05 04:54:08 +0300

    updates

--- config/plugins/code_runner.nix
+++ config/plugins/code_runner.nix
@@ -33,7 +33,7 @@
       filetype = {
         java = 'cd $dir && javac $fileName && java $fileNameWithoutExt',
         python = 'cd $dir && python3 $fileName',
-        hs = 'cd $dir && ghc $fileName',
+        haskell = 'cd $dir && runhaskell $fileName',
         r = 'Rscript $dir/$fileName',
         typescript = 'deno run',
         julia = 'julia $dir/$fileName',
