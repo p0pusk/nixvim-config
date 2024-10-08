b32b41d5d3be5e26842a06b653d3908a75038049
cringulya <s0sis0n@yandex.ru>
2024-10-08 19:30:26 +0300

    updates

--- config/plugins/cmake.nix
+++ config/plugins/cmake.nix
@@ -9,6 +9,7 @@
         vim.keymap.set('n', '<leader>cg', '<CMD>CMakeGenerate<CR>')
         vim.keymap.set('n', '<leader>cb', '<CMD>CMakeBuild<CR>')
         vim.keymap.set('n', '<leader>cr', '<CMD>CMakeRun<CR>')
+        vim.keymap.set('n', '<leader>ct', '<CMD>CMakeSelectLaunchTarget<CR>')
       end
     '';
   }];
